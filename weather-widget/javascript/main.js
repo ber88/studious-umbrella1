f245071ab651f2fc8021412834a85c7ccd70f608
ber88 <br2741@columbia.edu>
2023-07-01 11:22:43 -0400

    commit

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -2,7 +2,7 @@ console.log("this works");
 alert("Hello, WOrld!");
 const API_KEY = config.WEATHER_API_KEY;
 function getWeatherData() 
-    fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=${WEATHER_API_KEY}')
+    fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=685af0cca148a761850b28218bcbb704')
         .then(response => response.json())
         .then(data => console.log(data));
         
