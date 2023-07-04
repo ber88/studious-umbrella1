7f60acd271f38377e083942e589588a3711ce131
ber88 <br2741@columbia.edu>
2023-07-04 13:19:04 -0400

    C

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,8 +1,11 @@
-console.log("this works");
-alert("Hello, WOrld!");
 const API_KEY = config.WEATHER_API_KEY;
-function getWeatherData() 
+
+function getWeatherData() {
     fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=685af0cca148a761850b28218bcbb704')
-        .then(response => response.json())
-        .then(data => console.log(data));
-        
+        .then((response) => response.json())
+        .then((data) => {
+            let local_weather_data = data;
+                console.log(local_weather_data);
+});
+}
+getWeatherData()
